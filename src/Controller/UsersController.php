302cd7e217fbde103848c9dbf7449f06a05b1a95
Dmitry Neverovski <dmitryneverovski@gmail.com>
2018-04-07 15:49:54 +0300

    Adding the actual user registration logic in UsersController

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,15 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Exception\ValidationException;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
 
 /**
  * @Security("is_anonymous() or is_authenticated()")
@@ -38,29 +38,33 @@ class UsersController extends AbstractController
     }
 
     /**
-     * @param Request $request
-     * @return JsonResponse
-     * @throws \Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTEncodeFailureException
+     * @Rest\View(statusCode=201)
+     * @ParamConverter(
+     *     "user",
+     *     converter="fos_rest.request_body",
+     *     options={"deserializationContext"={"groups"={"Deserialize"}}}
+     * )
+     * @Rest\NoRoute()
      */
-    public function postUserAction(Request $request)
+    public function postUserAction(User $user, ConstraintViolationListInterface $validationErrors)
     {
-        $user = $this->getDoctrine()->getRepository('App:User')->findOneBy(['username' => $request->getUser()]);
-        if (!$user) {
-            throw new BadCredentialsException();
+        if (count($validationErrors) > 0) {
+            throw new ValidationException($validationErrors);
         }
 
-        $isPasswordValid = $this->passwordEncoder->isPasswordValid($user, $request->getPassword());
-
-        if (!$isPasswordValid) {
-            throw new BadCredentialsException();
-        }
-
-        $token = $this->jwtEncoder->encode(
-            [
-                'username' => $user->getUsername(),
-                'time' => time() + 3600
-            ]
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword(
+                $user,
+                $user->getPassword()
+            )
         );
-        return new JsonResponse(['token' => $token]);
+
+        $user->setRoles([User::ROLE_USER]);
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($user);
+        $manager->flush();
+
+        return $user;
     }
 }
