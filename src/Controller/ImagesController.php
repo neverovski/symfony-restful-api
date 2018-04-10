5d3956a3426e7b65f503c470222d3836ca4be454
Dmitry Neverovski <dmitryneverovski@gmail.com>
2018-04-10 14:21:01 +0300

    Uploading an image (PUT endpoint)

--- src/Controller/ImagesController.php
+++ src/Controller/ImagesController.php
@@ -3,15 +3,49 @@
 namespace App\Controller;
 
 use App\Entity\Image;
+use App\Repository\ImageRepository;
 use FOS\RestBundle\Controller\ControllerTrait;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\MimeType\ExtensionGuesser;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
 
 class ImagesController extends AbstractController
 {
     use ControllerTrait;
+    /**
+     * @var ImageRepository
+     */
+    private $imageRepository;
+    /**
+     * @var string
+     */
+    private $imageDirectory;
+    /**
+     * @var string
+     */
+    private $imageBaseUrl;
+
+    /**
+     * ImagesController constructor.
+     * @param ImageRepository $imageRepository
+     * @param string $imageDirectory
+     * @param string $imageBaseUrl
+     */
+    public function __construct(
+        ImageRepository $imageRepository,
+        string $imageDirectory,
+        string $imageBaseUrl
+    )
+    {
+        $this->imageRepository = $imageRepository;
+        $this->imageDirectory = $imageDirectory;
+        $this->imageBaseUrl = $imageBaseUrl;
+    }
 
     /**
      * @ParamConverter(
@@ -23,10 +57,7 @@ class ImagesController extends AbstractController
      */
     public function postImagesAction(Image $image)
     {
-        $manager = $this->getDoctrine()->getManager();
-
-        $manager->persist($image);
-        $manager->flush();
+        $this->persistImage($image);
 
         return $this->view($image, Response::HTTP_CREATED)->setHeader(
             'Location',
@@ -35,5 +66,59 @@ class ImagesController extends AbstractController
                 ['images' => $image->getId()]
             )
         );
+    }
+
+    /**
+     * @Rest\NoRoute()
+     */
+    public function putImageUploadAction(?Image $image, Request $request)
+    {
+        if (null === $image) {
+            throw new NotFoundHttpException();
+        }
+
+        // Read the image content from request body
+        $content = $request->getContent();
+        // Create the temporary upload file (deleted after request finishes)
+        $tmpFile = tmpfile();
+        // Get the temporary file name
+        $tmpFilePatch = stream_get_meta_data($tmpFile)['path'];
+        // Write image content to the temporary file
+        file_put_contents($tmpFilePatch, $content);
+
+        // Get the file mime-type
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mimeType = finfo_file($finfo, $tmpFilePatch);
+
+        // Check if it's really an image (never trust client set mime-type!)
+        if (!in_array($mimeType, ['image/jpeg', 'image/png', 'image/gif'])) {
+            throw new UnsupportedMediaTypeHttpException(
+                'File uploaded is not a valid png/jpeg/gif image'
+            );
+        }
+
+        // Guess the extension based on mime-type
+        $extensionGuesser = ExtensionGuesser::getInstance();
+        // Generate a new random filename
+        $newFileName = md5(uniqid()).'.'.$extensionGuesser->guess($mimeType);
+
+        // Copy the temp file to the final uploads directory
+        copy($tmpFilePatch, $this->imageDirectory.DIRECTORY_SEPARATOR.$newFileName);
+
+        $image->setUrl($this->imageBaseUrl.$newFileName);
+
+        $this->persistImage($image);
+
+        return new Response(null, Response::HTTP_OK);
+    }
+
+    /**
+     * @param Image|null $image
+     */
+    public function persistImage(?Image $image): void
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($image);
+        $manager->flush();
     }
 }
